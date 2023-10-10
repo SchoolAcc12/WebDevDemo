b7bd80b07c09b10d06586c214728b8f730ae2d96
StevenSon12 <stivanstf55@gmail.com>
2023-10-10 14:52:14 +0100

    Set up react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,20 @@
 import './styles/App.css'
 import Register from './Register'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <>
-      <div>
-        <Register />
-      </div>    
-    </>
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path='/' element={<Register />}>
+            <Route index element={<Register />} />
+            <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
