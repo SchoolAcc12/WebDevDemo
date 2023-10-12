8745addd41f301fd1bab168d7cbcf7749ac75ade
StevenSon12 <stivanstf55@gmail.com>
2023-10-12 14:03:27 +0100

    SOLVED. We can now add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,19 @@
 import './styles/App.css'
 import Register from './Register'
 import HomePage from './HomePage'
-import Layout from './Layout'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
 
   return (
-    <BrowserRouter>
-    
-      <Routes>
-        <Route path='/' element={<Layout />}>
-            <Route index element={<HomePage/>} />
-            <Route path="register" element={<Register />} />
-        </Route>
-      </Routes>
-
-    </BrowserRouter>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/">
+        <Route index element={<HomePage />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
   )
 }
 
