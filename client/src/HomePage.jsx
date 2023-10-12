b8943cf30e24073b0d12cefeb820e876ef4fdd2f
StevenSon12 <stivanstf55@gmail.com>
2023-10-12 17:22:42 +0100

    Made some progress on the landing page

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -1,4 +1,5 @@
 import './styles/HomePage.css'
+import hotelImg from './assets/hotel.jpg'
 
 function NavBar(){
     return(
@@ -15,10 +16,41 @@ function NavBar(){
             </div>
 
             <div className='signupbut'>
-                <button id="signupbut">Sign up</button>
+                <button id="signupbut">Sign Up</button>
             </div>
 
         </nav>
+    );
+}
+
+function MainPage(){
+    return(
+        <div className='main-page'>
+
+            <div className='left-section'>
+                <h1 id="title">Build a trip you trust</h1>
+                <p id="titlep">World Hotel saves you time and money on expenses,
+                    invoices and makes booking a trip as easy as clicking
+                    a few buttons.
+                </p>
+                
+                <div className='get-started'>
+                    <input id="emailinput"
+                    placeholder='Your e-mail'></input>
+
+                    <button id="get-started-but">Get Started</button>
+                </div>
+
+                <div className='destinations-info'>
+                    
+                </div>
+            </div>
+
+            <div className='right-section'>
+                <img src={hotelImg} width="800px" height="700px"></img>
+            </div>
+
+        </div>
     );
 }
 
@@ -26,6 +58,9 @@ function NavBar(){
 export default function HomePage(){
 
     return(
-        <NavBar />
+        <div className='home-page'>
+            <NavBar />
+            <MainPage />
+        </div>
     );
 }
