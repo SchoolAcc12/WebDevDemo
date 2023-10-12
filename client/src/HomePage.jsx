48c423ca3791990d75386f46099f5052eefdab83
StevenSon12 <stivanstf55@gmail.com>
2023-10-12 16:51:21 +0100

    Irresponsive navbar done

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -1,8 +1,31 @@
 import './styles/HomePage.css'
 
+function NavBar(){
+    return(
+        <nav className='navbar'>
+
+            <div className='logo'>
+                <strong><p id="logo">WorldHotels.</p></strong>
+            </div>
+
+            <div className='links'>
+                <a>Home</a>
+                <a>About</a>
+                <a>FAQ</a>
+            </div>
+
+            <div className='signupbut'>
+                <button id="signupbut">Sign up</button>
+            </div>
+
+        </nav>
+    );
+}
+
 
 export default function HomePage(){
+
     return(
-        <div>World Hotels</div>
+        <NavBar />
     );
 }
